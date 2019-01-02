de35133abb8322446e6cf6ef73ed264abbbf21e8
Johnn Mis <inrumi@gmail.com>
2019-01-02 16:33:46 -0500

    Almost finished, missing: form & footer

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-console.log('heyooss');
+/*
+const titleList = document.querySelectorAll('.mission-title-list-item');
+let current = 0;
 
+setInterval(
+    () => {
+      titleList[current].classList.remove('mission-title-list-item--show');
+      current = (current + 1) % titleList.length;
+      titleList[current].classList.add('mission-title-list-item--show');
+    },
+    2500
+);
+*/
+
+const garmFormItems = document.querySelectorAll('.garm-form-item');
+
+for (let formItem of garmFormItems) {
+  formItem.children[0].addEventListener('input', function () {
+    if (!this.value) return;
+    formItem.children[1].classList.add('label--active')
+  });
+
+  if (formItem.children[0].value) {
+    formItem.children[1].classList.add('label--active')
+  }
+}
